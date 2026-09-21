09354b2b20617603411a7d766ea3959e160dfd30
اسمك <ghgd705@gmil.com>
2026-09-21 03:37:18 +0300

    Configure Aiven SSL for Vercel runtime

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/sh
+set -e
+
+if [ -n "${AIVEN_CA_BASE64:-}" ]; then
+    printf '%s' "$AIVEN_CA_BASE64" | base64 -d > /tmp/aiven-ca.pem
+    chmod 600 /tmp/aiven-ca.pem
+    export MYSQL_ATTR_SSL_CA=/tmp/aiven-ca.pem
+fi
+
+exec "$@"
